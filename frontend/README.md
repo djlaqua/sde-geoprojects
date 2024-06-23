373d81eb3fa2d37bc5bd21f5665da7a1448c5c57
djlaqua <58705730+djlaqua@users.noreply.github.com>
2024-06-23 19:10:14 +0200

    Codebase changes & file additions

---
+++ frontend/README.md
@@ -0,0 +1,7 @@
+Data was generated from: https://geojson.io/#new&map=10.9/52.5384/13.4103 - AOI in Berlin, Germany
+
+https://jsonformatter.curiousconcept.com/ - can be used for proper json file formatting FYI
+
+Created & then formatted GeoJSon file and renamed to test.js to ensure compatibility with the html index file.
+
+var data = added to the collapsed test.js file from row 1
--- frontend/data/test.js
+++ frontend/data/test.js
@@ -1,357 +1,356 @@
 var data = {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": { name: 'testing' },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          70.609130859375,
-          38.955137225429574
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          68.4832763671875,
-          37.57505900514996
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          72.57568359375,
-          38.886757140695906
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          72.872314453125,
-          38.487994609214795
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          72.1527099609375,
-          38.90813299596705
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          71.7901611328125,
-          39.01918369029134
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          73.3721923828125,
-          39.13432124527173
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          72.916259765625,
-          39.23650795487107
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          73.10302734375,
-          38.9380483825641
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          71.9879150390625,
-          38.50519140240356
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          71.1309814453125,
-          38.655488159953
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          71.3067626953125,
-          39.138581990583525
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          71.510009765625,
-          38.77121637244273
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          70.0103759765625,
-          38.43207668538207
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          70.46630859375,
-          39.29604824402406
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          69.8236083984375,
-          39.02345139405935
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          72.388916015625,
-          38.35888785866677
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          72.5701904296875,
-          38.6897975322717
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          72.0208740234375,
-          37.97451499202459
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          71.56494140625,
-          38.238180119798635
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          73.092041015625,
-          37.965854128749434
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          73.4600830078125,
-          38.565347844885466
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          72.41638183593749,
-          37.58376576718623
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          68.7579345703125,
-          38.976492485539396
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          70.565185546875,
-          38.59540719940386
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          70.9442138671875,
-          38.69408504756833
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          71.05957031249999,
-          39.00211029922515
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          70.7574462890625,
-          39.3130504637139
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          69.76318359375,
-          38.11727165830543
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          68.994140625,
-          38.74551518488265
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          69.093017578125,
-          38.182068998322094
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          73.8006591796875,
-          38.00049145082287
-        ]
-      }
-    }
-  ]
-}
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Peter Panzer"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.327051151785184,
+                    52.51600546526083
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Bruno Banani"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.405768266514286,
+                    52.543898571763464
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Kurt Ketchup"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.415287974475007,
+                    52.53856214948246
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Heinz Hering"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.391238874334107,
+                    52.53353780752846
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Peter Panzer"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.414035704555744,
+                    52.523174674087045
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Otto Normalverbraucher"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.389983548495707,
+                    52.5053361325144
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Wolfgang Wasserfall"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.442842950685076,
+                    52.4739164064234
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Bruno Banani"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.433593070971256,
+                    52.527259048503595
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Fritz Blitz"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.368500499200564,
+                    52.546820640508656
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Heinz Hering"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.326238356512022,
+                    52.49032743786057
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Bruno Banani"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.293558084172673,
+                    52.5010617494716
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Bruno Banani"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.448381721087998,
+                    52.51068884631448
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Wolfgang Wasserfall"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.353194283465172,
+                    52.49426866145811
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Wolfgang Wasserfall"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.374206586112422,
+                    52.563061595952064
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Fritz Blitz"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.39506047093451,
+                    52.484885868242685
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Kurt Ketchup"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.409915210765377,
+                    52.5578256991397
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Wolfgang Wasserfall"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.438838609290428,
+                    52.54689077843943
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Karl-Heinz Br\u00f6tchen"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.369260096385688,
+                    52.481552629467274
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Otto Normalverbraucher"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.351534088729125,
+                    52.57256433302166
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Kurt Ketchup"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.254925057977687,
+                    52.51534907060838
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Otto Normalverbraucher"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.52742936612239,
+                    52.53278580596185
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Fritz Blitz"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.483119033711375,
+                    52.556864264953276
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Hans Wurst"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.433375570944065,
+                    52.496937689807396
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Karl-Heinz Br\u00f6tchen"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.313211237294581,
+                    52.5121720556636
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Heinz Hering"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.371337932817795,
+                    52.44883299535033
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Bruno Banani"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.309336975362044,
+                    52.465052418251645
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "names_in_all": "Otto Normalverbraucher"
+            },
+            "geometry": {
+                "coordinates": [
+                    13.486603449576734,
+                    52.50689420364907
+                ],
+                "type": "Point"
+            }
+        }
+    ]
+};
